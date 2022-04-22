ea0f948be36e7aace256077336c31ae43433e850
13929520142 <wangqy94@163.com>
2022-04-22 14:49:05 +0800

    修正

--- src/main/java/com/example/demo/controller/BucketController.java
+++ src/main/java/com/example/demo/controller/BucketController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/*
+    限流，采用令牌桶算法
+ */
 @RestController
 @RequestMapping("/bucket")
 public class BucketController {
